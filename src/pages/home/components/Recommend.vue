6ea0ee2859831dea6177f32b6a6583280cb1eee3
shenqz-web <308178853@qq.com>
2018-04-25 16:40:51 +0800

    add recommend weekend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,91 @@
+<template lang="html">
+  <div class="">
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{ item.title }}</p>
+          <p class="item-desc">{{ item.desc }}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl: 'https://imgs.qunarzz.com/sight/p0/1511/2e/2e3c8d254fdee61690.water.jpg_110x110_1fc9f714.jpg',
+          title: '当当城儿童职业体验馆',
+          desc: '[周末/节假日]御龙温泉成人票（含水上乐园）'
+        }, {
+          id: '0002',
+          imgUrl: 'https://imgs.qunarzz.com/sight/p0/1511/2e/2e3c8d254fdee61690.water.jpg_110x110_1fc9f714.jpg',
+          title: '当当城儿童职业体验馆',
+          desc: '[周末/节假日]御龙温泉成人票（含水上乐园）'
+        }, {
+          id: '0003',
+          imgUrl: 'https://imgs.qunarzz.com/sight/p0/1511/2e/2e3c8d254fdee61690.water.jpg_110x110_1fc9f714.jpg',
+          title: '当当城儿童职业体验馆',
+          desc: '[周末/节假日]御龙温泉成人票（含水上乐园）'
+        }, {
+          id: '0004',
+          imgUrl: 'https://imgs.qunarzz.com/sight/p0/1511/2e/2e3c8d254fdee61690.water.jpg_110x110_1fc9f714.jpg',
+          title: '当当城儿童职业体验馆',
+          desc: '[周末/节假日]御龙温泉成人票（含水上乐园）'
+        }, {
+          id: '0005',
+          imgUrl: 'https://imgs.qunarzz.com/sight/p0/1511/2e/2e3c8d254fdee61690.water.jpg_110x110_1fc9f714.jpg',
+          title: '当当城儿童职业体验馆',
+          desc: '[周末/节假日]御龙温泉成人票（含水上乐园）'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    display: flex
+    height: 1.9rem
+    margin: .1rem 0
+    overflow: hidden
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        margin-top: .2rem
+        background: #ff9300
+        padding: .05rem .2rem
+        border-radius: .06rem
+        color: #fff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,64 @@
+<template lang="html">
+  <div class="">
+    <div class="title">周末去哪</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{ item.title }}</p>
+          <p class="item-desc">{{ item.desc }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/c2/0be94f64d9df91.jpg_r_640x214_130f7614.jpg',
+          title: '当当城儿童职业体验馆',
+          desc: '[周末/节假日]御龙温泉成人票（含水上乐园）'
+        }, {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/c2/0be94f64d9df91.jpg_r_640x214_130f7614.jpg',
+          title: '当当城儿童职业体验馆',
+          desc: '[周末/节假日]御龙温泉成人票（含水上乐园）'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item-img-wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 33.9%
+    .item-img
+      width: 100%
+  .item-info
+    padding: .1rem
+    .item-title
+      line-height: .48rem
+      font-size: .28rem
+      ellipsis()
+    .item-desc
+      line-height: .3rem
+      color: #ccc
+      ellipsis()
+</style>
