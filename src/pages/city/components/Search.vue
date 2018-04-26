a2e5e4ca75f5c92b7c8b4bb8d8caaceaf39ba1f0
shenqz-web <308178853@qq.com>
2018-04-26 14:18:59 +0800

    use vuex

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -10,7 +10,12 @@
     </div>
     <div class="search-content" ref="search" v-show="keyword">
       <ul>
-        <li class="search-item border-bottom" v-for="item of list" :key="item.id">
+        <li
+          class="search-item border-bottom"
+          v-for="item of list"
+          :key="item.id"
+          @click="handleCityClick(item.name)"
+        >
           {{ item.name }}
         </li>
         <li class="search-item border-bottom" v-show="hasNoData">沒有匹配数据</li>
@@ -31,6 +36,12 @@ export default {
       keyword: '',
       list: [],
       timer: null
+    }
+  },
+  methods: {
+    handleCityClick (city) {
+      this.$store.commit('changeCity', city)
+      this.$router.push('/')
     }
   },
   computed: {
